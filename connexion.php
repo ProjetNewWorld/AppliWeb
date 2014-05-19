dbc689e549dfb4ad4ac2853c978365afe1fbaeb6
Nicolas Capiaumont <nicolas.capiaumont@gmail.com>
2014-05-19 20:10:03 +0200

    page de connexion pour entrer sur l'application

--- connexion.php
+++ connexion.php
@@ -1,11 +1,18 @@
 <!doctype html>
 <html lang="fr">
 <head>
-  <meta charset="utf-8">
-  <title>Connexion</title>
-  <link rel="stylesheet" href="style/style.css">
+	<meta charset="utf-8">
+	<title>Connexion</title>
+	<link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-
+	<h1>Connexion</h1>
+	<form name="connexion" method="" action="">
+		Identifiant<input type="text" name="user_identifiant">
+		Mot de Passe<input type="password" name="user_pwd">
+		<input type="submit" value="Connexion">
+	</form>
+	<a href="inscription.php">S'inscrire</a>
+	<a href="">Mot de passe oublié</a>
 </body>
 </html>
