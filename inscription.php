9cfb473f4473912cec5b2984f16885b5492d1a7a
Nicolas Capiaumont <nicolas.capiaumont@gmail.com>
2014-05-19 20:08:56 +0200

    ajout formulaire qui permet de s'enregistrer

--- inscription.php
+++ inscription.php
@@ -1,11 +1,20 @@
 <!doctype html>
 <html lang="fr">
 <head>
-  <meta charset="utf-8">
-  <title>Inscription</title>
-  <link rel="stylesheet" href="style/style.css">
+	<meta charset="utf-8">
+	<title>Inscription</title>
+	<link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-
+	<h1>Inscription</h1>
+	<form name="connexion" method="" action="">
+		Nom<input type="text" name="new_user_nom">
+		Prenom<input type="text" name="new_user_prenom">
+		Email<input type="email" name="new_user_email">
+		Identifiant<input type="text" name="new_user_login">
+		Mot de Passe<input type="password" name="new_user_pwd1">
+		Retapez votre mot de passe<input type="password" name="new_user_pwd2">
+		<input type="submit" value="Enregistrer">
+	</form>
 </body>
 </html>
