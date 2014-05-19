1b98a10bdbe852b1aac23343ecec38546f7d840a
Nicolas Capiaumont <nicolas.capiaumont@gmail.com>
2014-05-19 20:10:25 +0200

    ajout d'un formulaire pour créer une famille

--- creationFamille.php
+++ creationFamille.php
@@ -1,11 +1,15 @@
 <!doctype html>
 <html lang="fr">
 <head>
-  <meta charset="utf-8">
-  <title>Créer une famille</title>
-  <link rel="stylesheet" href="style/style.css">
+	<meta charset="utf-8">
+	<title>Créer une famille</title>
+	<link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-
+	<h1>Création d'une famille</h1>
+	<form name="connexion" method="" action="">
+		Nom de la famille<input type="text" name="new_famille_nom">
+		<input type="submit" value="Enregistrer">
+	</form>
 </body>
 </html>
