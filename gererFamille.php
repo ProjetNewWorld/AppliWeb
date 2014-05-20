0bc4b2f8fe1eced43c44719ae651ed0e8051d150
Capiaumont <nicolas.capiaumont@gmail.com>
2014-05-20 18:44:45 +0200

    ajout style dans gérer famille

--- gererFamille.php
+++ gererFamille.php
@@ -6,6 +6,19 @@
 	<link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-	<h1>Gérer sa famille</h1>
+    <header>
+		<div id="logo"></div>
+		<div id="titreHeader">
+			<div id="titreHeader-l1">Liste de courses</div>
+			<div id="titreHeader-l2">Projet New World</div>
+			<div id="titreHeader-l3">Les courses pour tous !</div>
+		</div>
+	</header>
+	<nav>
+		Accueil // BLABLA // BLABLA
+	</nav>
+	<div class="container">
+		<h1>Gérer sa famille</h1>
+    </div>
 </body>
 </html>
