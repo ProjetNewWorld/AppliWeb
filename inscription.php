b7cb1d1a6378862b9e4dcd884cfab0cc511fea9e
Capiaumont <nicolas.capiaumont@gmail.com>
2014-05-20 18:45:38 +0200

    style dans inscription

--- inscription.php
+++ inscription.php
@@ -6,21 +6,34 @@
 	<link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-	<h1>Inscription</h1>
-	<form name="fInscription" method="post" action="services.php">
-		Nom<input type="text" name="new_user_nom" />
-		Prenom<input type="text" name="new_user_prenom"/>
-		Email<input type="email" name="new_user_email"/>
-		Identifiant<input type="text" name="new_user_login"/>
-		Mot de Passe<input type="password" name="new_user_pwd1"/>
-		Retapez votre mot de passe<input type="password" name="new_user_pwd2"/>
-		<input type="submit" name="inscription" value="Enregistrer"/>
-	</form>
-		<?php
-	if(isset($_GET['errorPasswd']))
-	{
-		echo "<div id='error'>Les mots de passes ne correspondent pas</div>";
-	}
-	?>
+    <header>
+		<div id="logo"></div>
+		<div id="titreHeader">
+			<div id="titreHeader-l1">Liste de courses</div>
+			<div id="titreHeader-l2">Projet New World</div>
+			<div id="titreHeader-l3">Les courses pour tous !</div>
+		</div>
+	</header>
+	<nav>
+		Accueil // BLABLA // BLABLA
+	</nav>
+	<div class="container">
+        <h1>Inscription</h1>
+        <form name="fInscription" method="post" action="services.php">
+            Nom<input type="text" name="new_user_nom" />
+            Prenom<input type="text" name="new_user_prenom"/>
+            Email<input type="email" name="new_user_email"/>
+            Identifiant<input type="text" name="new_user_login"/>
+            Mot de Passe<input type="password" name="new_user_pwd1"/>
+            Retapez votre mot de passe<input type="password" name="new_user_pwd2"/>
+            <input type="submit" name="inscription" value="Enregistrer"/>
+        </form>
+            <?php
+        if(isset($_GET['errorPasswd']))
+        {
+            echo "<div id='error'>Les mots de passes ne correspondent pas</div>";
+        }
+        ?>
+	</div>
 </body>
 </html>
