04f82c27be8f3f8ed39bc6c991adc2ece6fe626e
Kevin Da Rocha <kev.darocha@gmail.com>
2014-05-20 15:58:00 +0200

    style de page de connexion

--- connexion.php
+++ connexion.php
@@ -5,14 +5,22 @@
 	<title>Connexion</title>
 	<link rel="stylesheet" href="style/style.css">
 </head>
-<body>
-	<h1>Connexion</h1>
-	<form name="connexion" method="" action="">
-		Identifiant<input type="text" name="user_identifiant">
-		Mot de Passe<input type="password" name="user_pwd">
-		<input type="submit" value="Connexion">
-	</form>
-	<a href="inscription.php">S'inscrire</a>
-	<a href="">Mot de passe oublié</a>
+<body class="bodyConnexion">
+	<div id="banniere">
+		<div id="logoConnexion">
+			<img src="style/img/logoListeCourse.png" width="100px"></img>
+		</div>
+		<div id="clearBoth"></div>
+		<h1 id="h1Connexion">Liste de course</h1>
+	</div>
+	<div id="blocConnexion">
+		<form id="formConnexion" name="connexion" method="" action="">
+			<p><label for="id">Identifiant :</label><input type="text" name="user_identifiant"></p>
+			<p><label for="id">Mot de Passe :</label><input type="password" name="user_pwd"></p>
+			<a href="inscription.php">S'inscrire</a>
+			<a href="">Mot de passe oublié</a>
+			<input type="submit" value="Connexion">
+		</form><!-- Fin du formulaire de connexion -->
+	</div><!-- Fin de la DIV blocConnexion -->
 </body>
 </html>
