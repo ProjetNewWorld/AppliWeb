272c2552ff8a113be512490bb3b4db6e51442ad1
Nicolas Capiaumont <nicolas.capiaumont@gmail.com>
2014-05-21 19:17:17 +0200

    ajout de variables + commentaires

--- commun.php
+++ commun.php
@@ -1,15 +1,23 @@
 <?php session_start();
-mysql_connect('127.0.0.1','userLDC','passLDC');
-
-mysql_select_db('listedescourses'); 
+//connection à la base de données
+//hote
+$host="127.0.0.1";
+//nom de l'utilisateur
+$user="userLDC";
+//le mot de passe de l'utilisateur
+$pass="passLDC";
+//nom base de données
+$db="listedescourses";
+mysql_connect($host,$user,$pass);
+//selection de la base de donnée
+mysql_select_db($db);
+//passage de la liste en cours à 0
 $noListeEnCours=0;
-
-
-if($connexionPage!=true)
+if($connexionPage!=true)//si la variable page de connexion n'est pas à vrai
 {
-	if(empty($_SESSION['login']))
+	if(empty($_SESSION['login']))//si la varibla session login est vide
 	{
-		header('Location: connexion.php');
+		header('Location: connexion.php');//redirection sur la page connexion.php
 	}
 }
 $connexionPage=false;
