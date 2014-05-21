e9c5256390109a69b8086656904660c664865b3b
Nicolas Capiaumont <nicolas.capiaumont@gmail.com>
2014-05-21 19:16:07 +0200

    ajout de nouvelles pages

---
+++ profil.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<?php 
+$connexionPage=false;
+include ("function.php");
+?>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Accueil</title>
+  <link rel="stylesheet" href="style/style.css">
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+  <script type="text/javascript" src="./script/addMembreToFamille.js"></script>
+  <link rel="icon" type="image/ico" href="style/img/favicon.ico"/>
+</head>
+<body>
+    <?php include("header.html") ?>
+	<nav>
+		Accueil // BLABLA // BLABLA
+	</nav>
+	<div class="container">
+		<h1>Gérer mon profil</h1>
+    </div>
+</body>
+</html>
