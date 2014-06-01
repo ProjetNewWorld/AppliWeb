7dc28c4ddb43260c2a02c928a9d1f1d668f94ebd
Nicolas Capiaumont <nicolas.capiaumont@gmail.com>
2014-06-01 18:22:19 +0200

    modification du style de gérer famille

--- gererFamille.php
+++ gererFamille.php
@@ -30,66 +30,70 @@ include('function.php');
 		//
 		<a href="connexion.php">Déconnexion</a>
 	</nav>
-	<div class="container">
 		<h1>Gérer sa famille</h1>
 		<?php
 		if(isChef())
 		{
 			?>
-			<h3>Ajouter un membre à ma famille : </h3>
-			<div class="milieu">
-				<p><input type="text" name="addMembreToFamille" id="pseudoAjouterALaFamille" placeholder='Pseudo du membre a ajouter' required/></p>
-				<p><input class="bouton" type="submit" id="ajouterALaFamille" value="Ajouter" /></p>
-			</div>
-			<div id="repAjout"></div>
-			<h3>Liste des personnes de votre famille (chef) : </h3> 
+		<h2>Ajouter un membre à ma famille : </h2>
+		<br>
+		<div class="center">
+			<p><input type="text" name="addMembreToFamille" id="pseudoAjouterALaFamille" placeholder='Pseudo du membre a ajouter' required/></p>
+			<p><input class="bouton" type="submit" id="ajouterALaFamille" value="Ajouter" /></p>
+		</div>
+		<div class="center" id="repAjout"></div>
+		<br>
+		<h2>Liste des personnes de votre famille (chef) : </h2>
+		<br>
+		<div class="center">
+		<?php
+		$membreFamilleId=getInfosMembreByLogin("familleId"); // on recupere l id de la famille du membre
+		$requette="select * from membre where familleId=$membreFamilleId";
+		$reponse=mysql_query($requette);
+		while($maLigne=mysql_fetch_array($reponse)) // pour tous les membres de la famille
+		{
+			?>
+			<div id="infoMemebreFamille"> 
+			<input id="getFamilleId" type="hidden" value="<?php echo $maLigne["familleId"]?>">
 			<?php
-					$membreFamilleId=getInfosMembreByLogin("familleId"); // on recupere l id de la famille du membre
-					$requette="select * from membre where familleId=$membreFamilleId";
-					$reponse=mysql_query($requette);
-					while($maLigne=mysql_fetch_array($reponse)) // pour tous les membres de la famille
-					{
-						?>
-						
-						<div id="infoMemebreFamille"> 
-						<input id="getFamilleId" type="hidden" value="<?php echo $maLigne["familleId"]?>">
-						<?php
-						$requetteAnnexe="select * from cheffamille where familleId=$membreFamilleId"; // on recupere l id du chef de famille
-						$reponseAnnexe=mysql_query($requetteAnnexe);
-						$ligneAnnexe=mysql_fetch_array($reponseAnnexe);
-						if($ligneAnnexe['membreId']==$maLigne["membreId"]) // si c est le chef de famille
-						{
-							echo "Chef de famille : ";
-							echo $maLigne["membreNom"]." ".$maLigne["membrePrenom"]." , pseudo : ".$maLigne["membreLogin"] ; // on affiche ses infos
-						}
-						else
-						{
-							echo $maLigne["membreNom"]." ".$maLigne["membrePrenom"]." , pseudo : ".$maLigne["membreLogin"] ; // sinon on affiche les infos et la possibilité de ban ou de promouvoir
-							?>
-							<button class="promouvoirMembre" value="<?php echo $maLigne["membreId"]?>"><img src="style/img/chef.png" alt="devenir chef" height="15"></button>
-							<button class="banMembre" value="<?php echo $maLigne["membreId"]?>"><img src="style/img/supprimer.png" alt="supprimer" height="15"></button>
-						<?php } ?>
-						</div>
-						<?php
-					}
+			$requetteAnnexe="select * from cheffamille where familleId=$membreFamilleId"; // on recupere l id du chef de famille
+			$reponseAnnexe=mysql_query($requetteAnnexe);
+			$ligneAnnexe=mysql_fetch_array($reponseAnnexe);
+			if($ligneAnnexe['membreId']==$maLigne["membreId"]) // si c est le chef de famille
+			{
+				echo "Chef de famille : ";
+				echo $maLigne["membreNom"]." ".$maLigne["membrePrenom"]." , pseudo : ".$maLigne["membreLogin"] ; // on affiche ses infos
+			}
+			else
+			{
+				echo $maLigne["membreNom"]." ".$maLigne["membrePrenom"]." , pseudo : ".$maLigne["membreLogin"] ; // sinon on affiche les infos et la possibilité de ban ou de promouvoir
+				?>
+				<button class="promouvoirMembre" value="<?php echo $maLigne["membreId"]?>"><img src="style/img/chef.png" alt="devenir chef"></button>
+				<button class="banMembre" value="<?php echo $maLigne["membreId"]?>"><img src="style/img/supprimer.png" alt="supprimer"></button>
+			<?php } ?>
+			</div>
+			<?php
+			}
 			?>
 			<form name="fSupprimerFamille" method="post" action="services.php">
+				<br>
 				<input class="bouton" type="submit" name="supprimerFamille" value="Supprimer cette famille"/>
 			</form>
 			<?php
 		}
 		else
 		{
-			?><h4>Liste des personnes de votre famille : </h4> 
+			?><h2>Liste des personnes de votre famille : </h2>
+			<br>
+			<div class="center">
 			<?php
-					$membreFamilleId=getInfosMembreByLogin("familleId"); // on recupere l id de la famille du membre
-					$requette="select * from membre where familleId=$membreFamilleId";
-					$reponse=mysql_query($requette);
-					while($maLigne=mysql_fetch_array($reponse)) // pour tous les membres de la famille
-					{
-						?>
-						
-						<div id="infoMemebreFamille"> <?php
+				$membreFamilleId=getInfosMembreByLogin("familleId"); // on recupere l id de la famille du membre
+				$requette="select * from membre where familleId=$membreFamilleId";
+				$reponse=mysql_query($requette);
+				while($maLigne=mysql_fetch_array($reponse)) // pour tous les membres de la famille
+				{
+					?>
+					<div id="infoMemebreFamille"> <?php
 						$requetteAnnexe="select * from cheffamille where familleId=$membreFamilleId"; // on recupere l id du chef de famille
 						$reponseAnnexe=mysql_query($requetteAnnexe);
 						$ligneAnnexe=mysql_fetch_array($reponseAnnexe);
@@ -99,13 +103,15 @@ include('function.php');
 						}
 						echo $maLigne["membreNom"]." ".$maLigne["membrePrenom"]." , pseudo : ".$maLigne["membreLogin"] ; // on affiche les infos du membre
 						?>
-						</div>
-						<?php
-					}
-			?>
-			<form name="fQuitFamille" method="post" action="services.php">
-			<input class="bouton" type="submit" name="quitFamille" value="Quitter cette famille"/>
-			</form>
+					</div>
+					<?php
+				}
+				?>
+				<form name="fQuitFamille" method="post" action="services.php">
+					<br>
+					<input class="bouton" type="submit" name="quitFamille" value="Quitter cette famille"/>
+				</form>
+			</div>
 			<?php
 		}
 		?>
